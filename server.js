9a75c8778bf2e7dc45294e6be08fe3bcbc6f3ee5
bfv <dev@bfv.io>
2020-02-09 08:37:33 +0100

    version 1.0

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World! v1.0');
 });
 
 app.listen(3000, () => {
